0328defca50cc01f5dcf2ce531eba5c48d01b703
Douglas Conrad <conrad@opens.com.br>
2017-12-28 08:15:46 -0200

    - implemented the option to user insert a set of digits and pass it forward through the asterisk variable USERINPUT - implemented the check of different type of options the user can insert: menu option, extension option, a set of digits or an invalid menu option. - changed the absolute wait timeout for between digits timeout.

--- actions/IVR.php
+++ actions/IVR.php
@@ -74,7 +74,7 @@ class IVR extends PBX_Rule_Action {
      * @return String XML
      */
     public function getConfig() {
-        $userentry = (isset($this->config['userentry']))?"<value>{$this->config['userentry']}</value>":"";
+        $maxdigits = (isset($this->config['maxdigits']))?"<value>{$this->config['maxdigits']}</value>":"";
         $allow_exten_dial = (isset($this->config['allow_exten_dial']))?"<value>{$this->config['allow_exten_dial']}</value>":"";
         $finalflow = (isset($this->config['finalflow']))?"<value>{$this->config['finalflow']}</value>":"";
         $file = (isset($this->config['file']))?"<value>{$this->config['file']}</value>":"";
@@ -86,12 +86,12 @@ class IVR extends PBX_Rule_Action {
 
         $menu = array(
           "audio_file" => $this->i18n->translate('Menu Audio File'),
-          "userentry" => $this->i18n->translate('Wait for user input'),
+          "maxdigits" => $this->i18n->translate('How many digits are allowed to user input.'),
           "allow_exten_dial" => $this->i18n->translate('Allow forward to an Extension'),
-          "prefix" => $this->i18n->translate('IVR Prefix String - used to identify the next steps on IVR process'),
-          "wait_time" => $this->i18n->translate('Wait for how long - in seconds'),
-          "wait_digits" => $this->i18n->translate('Wait for which digits - split by ","'),
-          "goto" => $this->i18n->translate('If Digits is wrong, go to action ID'),
+          "prefix" => $this->i18n->translate('IVR Prefix - used to identify the next steps on IVR process. You will need a Route with this destiny + the user input: PREFIX-USER_INPUT'),
+          "wait_time" => $this->i18n->translate('Wait for how long between each digit - in seconds'),
+          "wait_digits" => $this->i18n->translate('What menu digits options are allowed - split by ","'),
+          "goto" => $this->i18n->translate('If informed Digits is not expected, go to action ID'),
           "loops" => $this->i18n->translate('How many loops')
         );
         return <<<XML
@@ -101,12 +101,12 @@ class IVR extends PBX_Rule_Action {
         <id>file</id>
         $file
     </audio>
-    <boolean>
-    	<id>userentry</id>
-    	<default>false</default>
-    	<label>{$menu['userentry']}</label>
-	$userentry
-    </boolean>
+    <int>
+    	<id>maxdigits</id>
+    	<default>1</default>
+    	<label>{$menu['maxdigits']}</label>
+	$maxdigits
+    </int>
     <string>
         <label>{$menu['prefix']}</label>
         <id>prefix</id>
@@ -122,7 +122,7 @@ class IVR extends PBX_Rule_Action {
     <int>
         <label>{$menu['wait_time']}</label>
         <id>timeout</id>
-        <default>6</default>
+        <default>2</default>
         $timeout
     </int>
     <string>
@@ -160,16 +160,17 @@ XML;
     		$this->log = $log;
 
     		$stop_flow = false;
+        $valid = false;
 
     		$db = Zend_Registry::get("db");
 
-        $log->info("Answering and playing audio file: " . $this->config['file']);
+        $log->info("Answering the at IVR");
         $asterisk->answer();
-    		//$asterisk->stream_file($this->config['file']);
 
   			if($stop_flow == true){
   				$asterisk->hangup();
   			}
+
         $loop = $this->config['wloops'];
         $digits = explode(",",$this->config['digits']);
         if($this->config['prefix']){
@@ -180,32 +181,49 @@ XML;
 
         if($this->config['allow_exten_dial'] == "true"){
           $conf = Zend_Registry::get('config');
-          $maxdigits = $conf->canais->peers_digits;
-        }else{
-          $maxdigits = 1;
+          $peer_digits = $conf->canais->peers_digits;
         }
+
+        $maxdigits = $this->config['maxdigits'];
 
         $log->info("Waiting for $maxdigits digits in: {$this->config['digits']} and {$this->config['allow_exten_dial']}");
 
         while($loop > 0){
-          if($this->config['userentry'] == "true"){
-              $interaction++;
-              $userinput_row = $asterisk->get_data($this->config['file'],$this->config['timeout']*1000,$maxdigits);
-              $userinput = $userinput_row['result'];
-              $log->info("User input option: [{$userinput}] in the [$interaction] interaction.");
-              $log->info("Result code [{$userinput_row['code']}] and status [{$userinput_row['data']}]");
-              if(in_array($userinput, $digits)){
-                $log->info("Redirecting call to option $userinput");
-                $asterisk->exec_goto('default',$prefix . $userinput,1);
-                break;
-              }elseif ($this->config['allow_exten_dial'] == "true") {
-                $log->info("Redirecting call to Extension $userinput");
-                $asterisk->exec_goto('default',$prefix . $userinput,1);
-                break;
-              }
+          $interaction++;
+          $userinput_row = $asterisk->get_data($this->config['file'],$this->config['timeout']*1000, $maxdigits);
+          $userinput = $userinput_row['result'];
+          $asterisk->exec("Set","USERINPUT=$userinput");
+          $input_count = strlen($userinput);
+          $log->info("User inserted $input_count digits: [{$userinput}] in the [$interaction] interaction.");
+          $log->debug("Result code [{$userinput_row['code']}] and status [{$userinput_row['data']}]");
+          if($input_count == 1 && in_array($userinput, $digits)){
+            $log->info("User inserted a single digit in the menu option. Redirecting call to option $userinput");
+            $asterisk->exec_goto('default',$prefix . $userinput,1);
+            throw new PBX_Rule_Action_Exception_StopExecution("End of this IVR step");
+            break;
+          }elseif ($this->config['allow_exten_dial'] == "true" && $peer_digits == $input_count) {
+            $log->info("User inserted a extension digit. Redirecting call to Extension $userinput");
+            $asterisk->exec_goto('default', $userinput, 1);
+            throw new PBX_Rule_Action_Exception_StopExecution("End of this IVR step");
+            break;
+          }elseif($this->config['maxdigits'] <= $input_count){
+            $log->info("User inserted a set of digits. Going to the next action");
+            $valid = true;
+            break;
           }
+
           $loop--;
-       }
+        }
+
+        $log->debug("Testing wrongaction and valid input: [{$this->config['wrongaction']}] [$valid]");
+        if(isset($this->config['wrongaction']) && !$valid){
+
+          $log->info("Going to wrong option: {$this->config['wrongaction']}");
+
+          throw new PBX_Rule_Action_Exception_GoTo($this->config['wrongaction'] - 1);
+
+        }
+
 
       }
 
